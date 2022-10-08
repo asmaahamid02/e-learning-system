9fa5e941c3c559988424cd7c1e230d6d5285aa37
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-09 01:32:14 +0300

    backend: added submitAssignmment function

--- elearning-server/app/Http/Controllers/AssignmentController.php
+++ elearning-server/app/Http/Controllers/AssignmentController.php
@@ -17,7 +17,7 @@ class AssignmentController extends Controller
         //validate the input data
         $validator = Validator::make($request->all(), [
             'title' => 'required',
-            'assignment_file' => 'required',
+            // 'assignment_file' => 'required',
             'deadline' => 'required|date|after:' . Carbon::now()->format('Y-m-d H:i:s'),
         ]);
 
@@ -30,20 +30,17 @@ class AssignmentController extends Controller
             ]);
         }
 
-        $decoded_file = $this->base64Decode($request->assignment_file);
-
         $assignment = new Assignment();
         $assignment->instructor_id = Auth::id();
         $assignment->title = $request->title;
         $assignment->deadline = $request->deadline;
 
-        $file_path = 'users/assignments/' . Auth::id() . '/' . $decoded_file['file_name'];
-        Storage::disk('public')->put($file_path, $decoded_file['file_data']);
+        //store the file
+        // Storage::disk('public')->put('users/assignments/' . $decoded_file['file_name'], $decoded_file['file_data']);
 
-        $assignment->file_path = $file_path;
 
         $assignment->save();
-        //return course data and success message
+
         return response()->json([
             'data' => $assignment,
             'message' => $assignment->title . ' created successfully',
@@ -66,5 +63,62 @@ class AssignmentController extends Controller
             'file_name' => $file_name,
             'file_data' => base64_decode($file_data)
         ];
+    }
+
+    public function getAssignment($file)
+    {
+        return response(asset('storage/users/assignments/' . $file), 200);
+    }
+
+    public function submitAssignment(Request $request, $id)
+    {
+        if (Auth::user()->role == 'student') {
+            $student_id = Auth::id();
+            $assignment = Assignment::where('_id', $id)->first();
+
+            if ($assignment && $assignment->deadline > Carbon::now()->format('Y-m-d H:i:s')) {
+
+                //validate the input data
+                $validator = Validator::make($request->all(), [
+                    'assignment_file' => 'required',
+                ]);
+
+                //return the validator errors
+                if ($validator->fails()) {
+                    return response()->json([
+                        'data' => $validator->errors(),
+                        'message' => 'Invalid Data',
+                        'status' => Response::HTTP_INTERNAL_SERVER_ERROR
+                    ]);
+                }
+
+                //submit assignment
+                //check if already submitted
+                if (!$assignment->students->contains($student_id)) {
+                    $assignment->students()->attach($student_id, ['assignment_file', $request->assignment_file]);
+
+                    return response()->json([
+                        'data' => Auth::user()->studentAssignments,
+                        'message' => 'You have submitted your assignment Successfully',
+                        'status' =>  Response::HTTP_OK
+                    ]);
+                }
+                return response()->json([
+                    'data' => $assignment,
+                    'message' => 'You have already submitted your assignment Successfully',
+                    'status' =>  Response::HTTP_OK
+                ]);
+            }
+            return response()->json([
+                'data' => null,
+                'message' => 'Assignment not found',
+                'status' =>  Response::HTTP_NOT_FOUND
+            ]);
+        }
+        return response()->json([
+            'data' => null,
+            'message' => 'Only students can submit assignments',
+            'status' =>  Response::HTTP_FORBIDDEN
+        ]);
     }
 }
