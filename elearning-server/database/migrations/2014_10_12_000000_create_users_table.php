62b9f60c5248a8c09384fd49d5a345ff00a585ca
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-08 12:28:20 +0300

    backend: modified user migration

--- elearning-server/database/migrations/2014_10_12_000000_create_users_table.php
+++ elearning-server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,21 +14,14 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('role')->default('student');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('users');
