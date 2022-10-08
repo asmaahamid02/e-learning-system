d481470493057a6d840c0c047c0804b77fe6f539
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-08 15:11:25 +0300

    backend: created addCourse function

--- elearning-server/app/Http/Controllers/CourseController.php
+++ elearning-server/app/Http/Controllers/CourseController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
 
 class CourseController extends Controller
 {
-    //
+    public function addCourse(Request $request)
+    {
+        //validate the input data
+        $validator = Validator::make($request->all(), [
+            'code' => 'required',
+            'title' => 'required',
+            'subject' => 'required',
+            'description' => 'required|max:300',
+            'credits_number' => 'required|numeric',
+            'price' => 'required',
+        ]);
+
+        //return the validator errors
+        if ($validator->fails()) {
+            return response()->json([
+                'data' => $validator->errors(),
+                'message' => 'Invalid Data',
+                'status' => Response::HTTP_INTERNAL_SERVER_ERROR
+            ]);
+        }
+
+        // create the new course   
+        $course = Course::create($request->all());
+
+        //return course data and success message
+        return response()->json([
+            'data' => $course,
+            'message' => $course->code . ' created successfully',
+            'status' =>  Response::HTTP_OK
+        ]);
+    }
 }
