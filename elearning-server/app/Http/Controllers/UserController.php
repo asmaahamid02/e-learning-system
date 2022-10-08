ceacea150f589e2e9fef9ee82da16505659758d6
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-08 14:48:39 +0300

    backend: added addUser function in UserController

---
+++ elearning-server/app/Http/Controllers/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+    public function addUser(Request $request)
+    {
+        //validate the input data
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:5|max:20',
+            'role' => 'required|in:student,instructor',
+            'dob' => 'required:date',
+            'phone_number' => 'required|min:10|regex:/^([0-9\s\-\+\(\)]*)$/',
+            'gender' => 'required',
+        ]);
+
+        //return the validator errors
+        if ($validator->fails()) {
+            return response()->json([
+                'data' => $validator->errors(),
+                'message' => 'Invalid Data',
+                'status' => Response::HTTP_INTERNAL_SERVER_ERROR
+            ]);
+        }
+
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
+
+        // create the new user   
+        $user = User::create($data);
+
+        //return user data and success message
+        return response()->json([
+            'data' => $user,
+            'message' => Str::ucfirst($user->role) . ' Registered Successfully',
+            'status' =>  Response::HTTP_OK
+        ]);
+    }
+}
