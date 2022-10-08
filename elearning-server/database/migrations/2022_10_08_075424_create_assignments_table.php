ebda31bbb8c9f694d7b8fcc580472ef203521652
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-08 12:31:28 +0300

    backend: created assignments migration

---
+++ elearning-server/database/migrations/2022_10_08_075424_create_assignments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssignmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assignments', function (Blueprint $table) {
+            $table->integer('instructor_id');
+            $table->string('title');
+            $table->string('file_name');
+            $table->date('deadline');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assignments');
+    }
+}
