6a5d2d2acffef8f86ce0c8dea66b01ece31905ca
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-08 15:01:14 +0300

    backend: created CourseModel

---
+++ elearning-server/app/Models/CourseModel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CourseModel extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'title',
+        'subject',
+        'description',
+        'credits_number',
+        'price',
+        'instructors',
+        'students'
+    ];
+}
