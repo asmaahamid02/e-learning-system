2404c2bcfe69614c1241377c65e277e871fc55eb
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-09 21:34:20 +0300

    frontend: added form validations to login form

--- elearning-frontend/src/App.js
+++ elearning-frontend/src/App.js
@@ -1,13 +1,54 @@
 import login from './assets/images/login.png'
 import './App.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faEnvelope,
   faLock,
   faLockOpen,
 } from '@fortawesome/free-solid-svg-icons'
+import FormInput from './Components/FormInput'
+import Button from './Components/Button'
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+  })
+  const [errors, setErrors] = useState({})
+  const [isValid, setIsValid] = useState(false)
+
+  const validate = (formValues) => {
+    const err = {}
+    if (!formValues.email) {
+      err.email = 'Email should not be empty'
+    } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+      err.email = 'Email is not valid'
+    }
+    if (!formValues.password) {
+      err.password = 'Password should not be empty'
+    } else if (formValues.password.length < 5) {
+      err.password = 'Password should be 5 or more characters'
+    }
+
+    return err
+  }
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value })
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    setErrors(validate(values))
+    setIsValid(true)
+  }
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0 && isValid) {
+      //submit the form
+    }
+  }, [errors])
+  console.log(values)
   return (
     <div className='login-container'>
       <div className='header'>
@@ -17,34 +58,36 @@ function App() {
         <img src={login} alt='education' />
       </div>
       <div className='login-form'>
-        <form>
-          <div className='input-box'>
-            <span className='prefix'>
-              <FontAwesomeIcon icon={faEnvelope} className='icon-green' />
-            </span>
-            <input
-              className='input'
-              type='email'
-              name='email'
-              id='email'
-              placeholder='john@example.com'
-            />
-          </div>
-          <div className='input-box'>
-            <span className='prefix'>
-              <FontAwesomeIcon icon={faLock} className='icon-green' />
-            </span>
-            <input
-              className='input'
-              type='password'
-              name='password'
-              id='password'
-              placeholder='Aa12@#$%'
-            />
-          </div>
-          <button className='btn btn-large btn-dark' id='btn-login'>
-            <FontAwesomeIcon icon={faLockOpen} className='icon-dark' />
-          </button>
+        <form onSubmit={onSubmit}>
+          <FormInput
+            icon={faEnvelope}
+            iconClass='icon-green'
+            iconSize='lg'
+            type='text'
+            name='email'
+            id='email'
+            placeholder='john@example.com'
+            onChange={onChange}
+            errorMessage={errors.email}
+          />
+          <FormInput
+            icon={faLock}
+            iconClass='icon-green'
+            iconSize='lg'
+            type='password'
+            name='password'
+            id='password'
+            placeholder='Aa19!@#$%'
+            onChange={onChange}
+            errorMessage={errors.password}
+          />
+          <Button
+            classes='btn btn-large btn-dark'
+            id='btn-login'
+            icon={faLockOpen}
+            iconClass='icon-white'
+            iconSize='xl'
+          />
         </form>
       </div>
     </div>
