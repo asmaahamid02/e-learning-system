da2b6dbf559655d2c7419d96d4acbe41e63cb3d4
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-09 21:32:03 +0300

    frontend: added FormInput component

---
+++ elearning-frontend/src/Components/FormInput.js
@@ -0,0 +1,30 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+const FormInput = (props) => {
+  return (
+    <div className='input-box'>
+      <div className='inner-input'>
+        <span className='prefix'>
+          <FontAwesomeIcon
+            icon={props.icon}
+            className={props.iconClass}
+            size={props.iconSize}
+          />
+        </span>
+        <input
+          className='input'
+          type={props.type}
+          name={props.name}
+          id={props.id}
+          placeholder={props.placeholder}
+          onChange={props.onChange}
+        />
+      </div>
+
+      <span className={`error-message ${!props.errorMessage && 'hide'}`}>
+        {props.errorMessage}
+      </span>
+    </div>
+  )
+}
+
+export default FormInput
