27a2062197dca4c79eb5f2a1d74785c92494b985
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-09 18:40:09 +0300

    frontend: added login form

--- elearning-frontend/src/App.js
+++ elearning-frontend/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import login from './assets/images/login.png'
+import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faEnvelope,
+  faLock,
+  faLockOpen,
+} from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='login-container'>
+      <div className='header'>
+        <h1 className='header-title'>Welcome to LearnIt</h1>
+      </div>
+      <div className='login-image'>
+        <img src={login} alt='education' />
+      </div>
+      <div className='login-form'>
+        <form>
+          <div className='input-box'>
+            <span className='prefix'>
+              <FontAwesomeIcon icon={faEnvelope} className='icon-green' />
+            </span>
+            <input
+              className='input'
+              type='email'
+              name='email'
+              id='email'
+              placeholder='john@example.com'
+            />
+          </div>
+          <div className='input-box'>
+            <span className='prefix'>
+              <FontAwesomeIcon icon={faLock} className='icon-green' />
+            </span>
+            <input
+              className='input'
+              type='password'
+              name='password'
+              id='password'
+              placeholder='Aa12@#$%'
+            />
+          </div>
+          <button className='btn btn-large btn-dark' id='btn-login'>
+            <FontAwesomeIcon icon={faLockOpen} className='icon-dark' />
+          </button>
+        </form>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
