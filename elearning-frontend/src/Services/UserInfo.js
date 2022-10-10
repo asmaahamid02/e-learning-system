4188611b376a9f6e2187e350b0e53f0ad5a89f8b
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-10 10:59:17 +0300

    frontend: added getName function

--- elearning-frontend/src/Services/UserInfo.js
+++ elearning-frontend/src/Services/UserInfo.js
@@ -12,6 +12,10 @@ class UserInfo {
     return JSON.parse(localStorage.getItem('user')).role
   }
 
+  getName() {
+    return JSON.parse(localStorage.getItem('user')).name
+  }
+
   setUser(user) {
     localStorage.setItem('user', JSON.stringify(user))
   }
