b9f5981b4b92449c231c6d92ba1bfb7b311709f8
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-10 10:58:13 +0300

    frontend: modified Navbar buttons functionality

--- elearning-frontend/src/Views/Navbar.js
+++ elearning-frontend/src/Views/Navbar.js
@@ -5,33 +5,24 @@ import { Navigate } from 'react-router-dom'
 const Navbar = (props) => {
   return (
     <div className='navbar flex center-vertical'>
-      <div
-        className='username'
-        onClick={() => {
-          return <Navigate replace to='/' />
-        }}
-      >
-        <FontAwesomeIcon icon={props.userIcon} size={props.iconSize} />
+      <div className='username' onClick={props.handleHomeClick}>
+        <FontAwesomeIcon icon={props.userIcon} size={props.userIconSize} />
         <span>{UserInfo.getName()}</span>
       </div>
       <ul className='list flex'>
         {props.items.map((item, index) => {
           return (
-            <li key={index} className={item.active && 'active'}>
+            <li key={index} className={item.active ? 'active' : ''}>
               {item.name}
             </li>
           )
         })}
       </ul>
-      <div
-        className='logout'
-        onClick={() => {
-          UserInfo.removeUser()
-          return <Navigate replace to='/' />
-        }}
-      >
-        <FontAwesomeIcon icon={faRightFromBracket} size='xl' />
+      <div className='logout' onClick={props.handleLogout}>
+        <FontAwesomeIcon icon={props.logoutIcon} size={props.logoutIconSize} />
       </div>
     </div>
   )
 }
+
+export default Navbar
