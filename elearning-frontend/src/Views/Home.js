050c76d5a290452df2717641310d7b4cc8b30e33
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-11 02:02:19 +0300

    frontend: added model and its useStates

--- elearning-frontend/src/Views/Home.js
+++ elearning-frontend/src/Views/Home.js
@@ -7,9 +7,13 @@ import Navbar from './Navbar'
 import Instructor from '../Components/Instructor'
 import Student from '../Components/Student'
 import Course from '../Components/Course'
+import ModalForm from '../Components/ModalForm'
 
-const Home = () => {
+const Home = (props) => {
   const [loggedInUser, setLoggedInUser] = useState(UserInfo.getUser())
+
+  const [modalProps, setModalProps] = useState(null)
+  const [showModal, setShowModal] = useState(false)
 
   const checkLoggedIn = () => {
     if (!loggedInUser) {
@@ -27,37 +31,51 @@ const Home = () => {
     setLoggedInUser(UserInfo.getUser())
   })
 
+  useEffect(() => {
+    console.log('changed')
+  }, [modalProps])
   if (!loggedInUser) {
     return <Navigate replace to='/' />
   }
 
-  let content = <Instructor />
+  let content = <Instructor setModalProps={setModalProps} />
   if (UserInfo.getRole() == 'instructor') {
     content = <Student />
   } else if (UserInfo.getRole() == 'student') {
     content = <Course />
   }
   return (
-    <div className='main-container'>
-      <div className='left-sidebar'>
-        <div className='header flex center-vertical logo'>
-          <img className='logo-img' src={logoImg} alt='logo' />
-          <h1 className='header-title'>LearnIt</h1>
+    <>
+      <div className='main-container'>
+        <div className='left-sidebar'>
+          <div className='header flex center-vertical logo'>
+            <img className='logo-img' src={logoImg} alt='logo' />
+            <h1 className='header-title'>LearnIt</h1>
+          </div>
+        </div>
+        <div className='middle-content'>
+          <Navbar
+            userIcon={faUser}
+            userIconSize='xl'
+            logoutIcon={faRightFromBracket}
+            logoutIconSize='xl'
+            setLoggedInUser={setLoggedInUser}
+          />
+          <div className='content'>
+            <div className='items'>{content}</div>
+          </div>
         </div>
       </div>
-      <div className='middle-content'>
-        <Navbar
-          userIcon={faUser}
-          userIconSize='xl'
-          logoutIcon={faRightFromBracket}
-          logoutIconSize='xl'
-          setLoggedInUser={setLoggedInUser}
+      {modalProps && modalProps.showModal && (
+        <ModalForm
+          title={modalProps.title}
+          inputs={modalProps.inputs}
+          buttonText={modalProps.buttonText}
+          setModalProps={setModalProps}
+          modalProps={modalProps}
         />
-        <div className='content'>
-          <div className='items'>{content}</div>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   )
 }
 
