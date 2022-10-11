c9eb19c2f359085733329d343c81137043d2a08d
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-11 23:02:55 +0300

    frontend: created courses api functions

---
+++ elearning-frontend/src/API/courses.js
@@ -0,0 +1,32 @@
+import { api } from './api'
+
+class Course {
+  async getCourses() {
+    try {
+      const response = await api.get('courses/')
+      return response.data
+    } catch (error) {
+      return error
+    }
+  }
+
+  async assignCourseToInstructor(instructor_id, course_id) {
+    try {
+      return await api.get(
+        `courses/assign/instructor/${instructor_id}/${course_id}`
+      )
+    } catch (error) {
+      return error
+    }
+  }
+
+  async addCourse(data) {
+    try {
+      return await api.post('courses/', data)
+    } catch (error) {
+      return error
+    }
+  }
+}
+
+export default new Course()
